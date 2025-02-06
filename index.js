0c7ff6663c3d9bfc5b73693901b8665af6f174fc
juancc9 <sicsound00@gmail.com>
2025-02-05 22:45:21 -0500

    "Importacion de las rutas de Inventario_ventas a Index.js"

--- index.js
+++ index.js
@@ -9,4 +9,20 @@ app.get('/doc', (req, res) => {
     res.render('documentacion.ejs'); 
   });
 
+  // FINANZAS_VENTA
+import  ListarFinanzasVenta  from './src/routes/FinanzasVentaroute.js'; //  ListarFinanzasVenta
+import  ActualizarFinanzasVenta  from './src/routes/FinanzasVentaroute.js';
+import  RegistrarFinanzasVenta  from './src/routes/FinanzasVentaroute.js';
+import  BuscarFinanzasVenta from './src/routes/FinanzasVentaroute.js'
+import  EliminarFinanzasVenta from './src/routes/FinanzasVentaroute.js'
+
+
+
+// FINANZAS_VENTA
+app.use(ListarFinanzasVenta);
+app.use(ActualizarFinanzasVenta);
+app.use(RegistrarFinanzasVenta);
+app.use/(BuscarFinanzasVenta);
+app.use(EliminarFinanzasVenta);
+
 
