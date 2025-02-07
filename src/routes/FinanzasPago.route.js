c72bafbb0ff65466109ffad333ce9ffbcb379629
juancc9 <sicsound00@gmail.com>
2025-02-06 21:36:15 -0500

    "Creacion de las routers de Finanzas (Pago) con sus respectivos métodos"

---
+++ src/routes/FinanzasPago.route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { ListarFinanzasPago, RegistrarFinanzasPago } from '../controllers/Finanzas_pagoController.js';
+import {ActualizarFinanzasPago} from '../controllers/Finanzas_pagoController.js';
+import { BuscarFinanzasPago } from '../controllers/Finanzas_pagoController.js';
+import { EliminarFinanzasPago } from '../controllers/Finanzas_pagoController.js';
+
+const router = express.Router();
+router.get('/FinanzasPago', ListarFinanzasPago);
+router.get('/FinanzasPago/:id', BuscarFinanzasPago);
+router.put('/FinanzasPago/:id', ActualizarFinanzasPago)
+router.post('/FinanzasPago', RegistrarFinanzasPago)
+router.delete('/FinanzasPago/:id', EliminarFinanzasPago);
+
+
+export default router;
