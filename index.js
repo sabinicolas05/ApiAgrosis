e19edded4f27672d050686fb179813b3a5815029
juancc9 <sicsound00@gmail.com>
2025-02-06 21:54:39 -0500

    "Importacion de las rutas de finanzas a index.js"

--- index.js
+++ index.js
@@ -45,6 +45,37 @@ import  BuscarTipoSensor from './src/routes/IoT_Tipo_Sensor.route.js'
 import  EliminarTipoSensor from './src/routes/IoT_Tipo_Sensor.route.js'
 
 
+// FINANZAS_VENTA
+import  ListarFinanzasVenta  from './src/routes/FinanzasVentaroute.js'; //  ListarFinanzasVenta
+import  ActualizarFinanzasVenta  from './src/routes/FinanzasVentaroute.js';
+import  RegistrarFinanzasVenta  from './src/routes/FinanzasVentaroute.js';
+import  BuscarFinanzasVenta from './src/routes/FinanzasVentaroute.js'
+import  EliminarFinanzasVenta from './src/routes/FinanzasVentaroute.js'
+
+
+// FINANZAS_RESIDUO
+import  ListarFinanzasResiduo  from './src/routes/FinanzasResiduoroute.js'; //  ListarFinanzasResiduo
+import  ActualizarFinanzasResiduo  from './src/routes/FinanzasResiduoroute.js';
+import  RegistrarFinanzasResiduo  from './src/routes/FinanzasResiduoroute.js';
+import  BuscarFinanzasResiduo from './src/routes/FinanzasResiduoroute.js'
+import  EliminarFinanzasResiduo from './src/routes/FinanzasResiduoroute.js'
+
+
+// FINANZAS_PRODUCCION
+import  ListarFinanzasProduccion  from './src/routes/Finanzas_produccionroute.js'; //  ListarFinanzasProduccion
+import  ActualizarFinanzasProduccion  from './src/routes/Finanzas_produccionroute.js';
+import  RegistrarFinanzasProduccion  from './src/routes/Finanzas_produccionroute.js';
+import  BuscarFinanzasProduccion from './src/routes/Finanzas_produccionroute.js'
+import  EliminarFinanzasProduccion from './src/routes/Finanzas_produccionroute.js'
+
+
+// FINANZAS_PAGO
+import  ListarFinanzasPago  from './src/routes/FinanzasPago.route.js'; //  ListarFinanzasPago
+import  ActualizarFinanzasPago  from './src/routes/FinanzasPago.route.js';
+import  RegistrarFinanzasPago  from './src/routes/FinanzasPago.route.js';
+import  BuscarFinanzasPago from './src/routes/FinanzasPago.route.js'
+import  EliminarFinanzasPago from './src/routes/FinanzasPago.route.js'
+
 
 
 app.use(express.json());
@@ -91,6 +122,36 @@ app.use(RegistrarTipoSensor);
 app.use/(BuscarTipoSensor);
 app.use(EliminarTipoSensor);
 
+
+// FINANZAS_VENTA
+app.use(ListarFinanzasVenta);
+app.use(ActualizarFinanzasVenta);
+app.use(RegistrarFinanzasVenta);
+app.use/(BuscarFinanzasVenta);
+app.use(EliminarFinanzasVenta);
+
+// FINANZAS_RESIDUO
+app.use(ListarFinanzasResiduo);
+app.use(ActualizarFinanzasResiduo);
+app.use(RegistrarFinanzasResiduo);
+app.use/(BuscarFinanzasResiduo);
+app.use(EliminarFinanzasResiduo);
+
+
+// FINANZAS_PRODUCCION
+app.use(ListarFinanzasProduccion);
+app.use(ActualizarFinanzasProduccion);
+app.use(RegistrarFinanzasProduccion);
+app.use/(BuscarFinanzasProduccion);
+app.use(EliminarFinanzasProduccion);
+
+
+// FINANZAS_PAGO
+app.use(ListarFinanzasPago);
+app.use(ActualizarFinanzasPago);
+app.use(RegistrarFinanzasPago);
+app.use/(BuscarFinanzasPago);
+app.use(EliminarFinanzasPago);
 
 
 
