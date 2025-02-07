979093afebd5277e78c5e5e09d0175a6a0b85a41
sabinicolas05 <nsabirojas@gmail.com>
2025-02-06 20:25:47 -0500

    termine de poner el index de la configuracion de los sensores

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ import express from 'express';
 const app = express();
 const router = express.Router(); 
 
+// CONFIGURACION_SENSORES
+
+import  ListarSensorConfig  from './src/routes/IoT_Coniguracion.route.js'; //  ListarSensorConfig
+import  ActualizarSensorConfig  from './src/routes/IoT_Coniguracion.route.js';
+import  RegistrarSensorConfig  from './src/routes/IoT_Coniguracion.route.js';
+import  BuscarSensorConfig from './src/routes/IoT_Coniguracion.route.js'
+import  EliminarSensorConfig from './src/routes/IoT_Coniguracion.route.js'
+
+app.use(express.json());
+// CONFIGURACION SENSOR
+app.use(ListarSensorConfig);
+app.use(ActualizarSensorConfig);
+app.use(RegistrarSensorConfig);
+app.use/(BuscarSensorConfig);
+app.use(EliminarSensorConfig);
 
 
-app.use(express.json()); 
 
 // Usar el router
 app.use(router);
