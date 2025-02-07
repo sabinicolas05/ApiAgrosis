ce80c2f3db96ac86046f10de420cd4406e95691d
juancc9 <sicsound00@gmail.com>
2025-02-06 21:56:52 -0500

    "Importacion de las rutas de Inventario a index,js"

--- index.js
+++ index.js
@@ -77,6 +77,37 @@ import  BuscarFinanzasPago from './src/routes/FinanzasPago.route.js'
 import  EliminarFinanzasPago from './src/routes/FinanzasPago.route.js'
 
 
+// INVENTARIO_HERRAMIENTA
+import  ListarInventarioHerramienta  from './src/routes/Inventario_herramienta.route.js'; //  ListarInventarioHerramienta
+import  ActualizarInventarioHerramienta  from './src/routes/Inventario_herramienta.route.js';
+import  RegistrarInventarioHerramienta  from './src/routes/Inventario_herramienta.route.js';
+import  BuscarInventarioHerramienta from './src/routes/Inventario_herramienta.route.js'
+import  EliminarInventarioHerramienta from './src/routes/Inventario_herramienta.route.js'
+
+
+// INVENTARIO_INSUMO
+import  ListarInventarioInsumo  from './src/routes/Inventario_isumo.route.js'; //  ListarInventarioInsumo
+import  ActualizarInventarioInsumo  from './src/routes/Inventario_isumo.route.js';
+import  RegistrarInventarioInsumo  from './src/routes/Inventario_isumo.route.js';
+import  BuscarInventarioInsumo from './src/routes/Inventario_isumo.route.js'
+import  EliminarInventarioInsumo from './src/routes/Inventario_isumo.route.js'
+
+
+// INVENTARIO_TIPO_HERRAMIENTA
+import  ListarTipoHerramienta  from './src/routes/Inventario_tipo_herramienta.route.js'; //  ListarTipoHerramienta
+import  ActualizarTipoHerramienta  from './src/routes/Inventario_tipo_herramienta.route.js';
+import  RegistrarTipoHerramienta  from './src/routes/Inventario_tipo_herramienta.route.js';
+import  BuscarTipoHerramienta from './src/routes/Inventario_tipo_herramienta.route.js'
+import  EliminarTipoHerramienta from './src/routes/Inventario_tipo_herramienta.route.js'
+
+
+// INVENTARIO_TIPO_INSUMO
+import  ListarTipoInsumo  from './src/routes/Inventario_tipo_insumo.route.js'; //  ListarTipoInsumo
+import  ActualizarTipoInsumo  from './src/routes/Inventario_tipo_insumo.route.js';
+import  RegistrarTipoInsumo  from './src/routes/Inventario_tipo_insumo.route.js';
+import  BuscarTipoInsumo from './src/routes/Inventario_tipo_insumo.route.js'
+import  EliminarTipoInsumo from './src/routes/Inventario_tipo_insumo.route.js'
+
 
 app.use(express.json());
 
@@ -153,6 +184,36 @@ app.use(RegistrarFinanzasPago);
 app.use/(BuscarFinanzasPago);
 app.use(EliminarFinanzasPago);
 
+// INVENTARIO_HERRAMIENTA
+app.use(ListarInventarioHerramienta);
+app.use(ActualizarInventarioHerramienta);
+app.use(RegistrarInventarioHerramienta);
+app.use/(BuscarInventarioHerramienta);
+app.use(EliminarInventarioHerramienta);
+
+
+// INVENTARIO_INSUMO
+app.use(ListarInventarioInsumo);
+app.use(ActualizarInventarioInsumo);
+app.use(RegistrarInventarioInsumo);
+app.use/(BuscarInventarioInsumo);
+app.use(EliminarInventarioInsumo);
+
+
+// INVENTARIO_TIPO_HERRAMIENTA
+app.use(ListarTipoHerramienta);
+app.use(ActualizarTipoHerramienta);
+app.use(RegistrarTipoHerramienta);
+app.use/(BuscarTipoHerramienta);
+app.use(EliminarTipoHerramienta);
+
+
+// INVENTARIO_TIPO_HERRAMIENTA
+app.use(ListarTipoInsumo);
+app.use(ActualizarTipoInsumo);
+app.use(RegistrarTipoInsumo);
+app.use/(BuscarTipoInsumo);
+app.use(EliminarTipoInsumo);
 
 
 
