f30720a7deb57d68809b63ab5040a63c9d34ae2d
juancc9 <sicsound00@gmail.com>
2025-02-14 15:57:57 -0500

    "Mejora en parametros y ruta"

--- src/routes/Autenticacion.route.js
+++ src/routes/Autenticacion.route.js
@@ -4,7 +4,7 @@ import { ValidarToken } from '../controllers/AutenticacionController.js';
 
 const router = express.Router();
 router.get('/login', ValidarUser);
-router.post('/token', ValidarToken);
+router.post('/', ValidarToken); //se usa para validar el token generado en http://localhost:4000/login, en headers-> token, no se usa como endpoint
 
 
 export default router;
